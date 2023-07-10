a36c2117c8c5f4a1cd43c0b8447583bc35fb13d9
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-07-10 13:37:36 -0600

    Uses default export
    Uses default export

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
-export const MainView = () => {
+const MainView = () => {
   const [movies, setMovies] = useState([
     {
       id: "647f90f816db8bdac05eb155",
@@ -56,3 +56,4 @@ export const MainView = () => {
     </div>
   );
 };
+export default MainView;
