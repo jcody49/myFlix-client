e7077979abf2ac8c9f48272e97f3d4e3fe0f3b7b
jcody49 <123919470+jcody49@users.noreply.github.com>
2023-07-18 20:48:11 -0600

    updates main-view and movie-view
    adds logout handling
    login persistance via local storage
    token checking

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,61 +4,55 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 
-
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
-  const [user, setUser] = useState(storedUser? storedUser : null);
-  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-
-
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
 
   useEffect(() => {
-    fetch("https://myflixmovieapp-3df5d197457c.herokuapp.com/movies")
-      .then((response) => response.json())
-      .then((data) => {
-        const moviesFromApi = data.map((doc) => {
-          return {
-            ImagePath: doc.ImagePath,
-            Title: doc.Title,
-            Description: doc.Description,
-            Genre: doc.Genre,
-            Director: doc.Director,
-            Featured: doc.Featured
-          };
-        });
-        
-        setMovies(moviesFromApi);
-      });
-  }, []);
-
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
-
-  //passes bearer authorization in the header HTTP requests
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
+    if (!token) return;
 
     fetch("https://myflixmovieapp-3df5d197457c.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then((response) => response.json())
-      .then((movies) => {
-        setMovies(movies);
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            Title: movie.Title,
+            ImagePath: movie.ImagePath,
+            Description: movie.Description,
+            Genre: {
+              Name: movie.Genre.Name
+            },
+            Director: {
+              Name: movie.Director.Name
+            },
+            Featured: movie.Featured.toString()
+          };
+        });
+        setMovies(moviesFromApi);
       });
-  }, [token]/*dependency array ensures fetch is called every time token changes*/);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -68,11 +62,10 @@ export const MainView = () => {
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
-  }
+  };
 
   return (
     <div>
-
       <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>
         Logout
       </button>
